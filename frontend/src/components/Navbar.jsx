efa676e0b83333d9d6b355ca196d4de44609f694
jaykant <jaykantmishra88@gmail.com>
2025-01-13 17:39:01 +0530

    adding admin link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -80,11 +80,18 @@ const Navbar = () => {
 
         {/* Admin/Doctor Panel Button */}
         <a
-          href="/admin-panel"
-          className="bg-blue-600 text-white font-medium py-2 px-4 rounded-full hover:bg-blue-700 transition duration-300 hidden md:block"
-        >
-          Admin/Doctor Panel
-        </a>
+  href="https://healthmate-adminpanel1.onrender.com"
+  target="_blank"
+  rel="noopener noreferrer"
+  onClick={(e) => {
+    e.preventDefault(); // Prevent default anchor behavior
+    window.open("https://healthmate-adminpanel1.onrender.com", "_blank", "noopener,noreferrer");
+  }}
+  className="bg-blue-600 text-white font-medium py-2 px-4 rounded-full hover:bg-blue-700 transition duration-300 hidden md:block"
+>
+  Admin/Doctor Panel
+</a>
+
 
         {/* Mobile Menu Icon */}
         <img
